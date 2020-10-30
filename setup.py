4784fb8f494cdeaf234f321a0d0a0b2541aaec76
yolabingo <yolabingo@gmail.com>
2020-10-29 18:40:09 -0600

    use psycopg2-binary

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requirements = [
     # We still need to use pt-2 unless pt-3 released on Fedora32
     # see: https://github.com/dbcli/pgcli/pull/1197
     "prompt_toolkit>=2.0.6,<4.0.0",
-    "psycopg2 >= 2.8",
+    "psycopg2-binary >= 2.8",
     "sqlparse >=0.3.0,<0.4",
     "configobj >= 5.0.6",
     "pendulum>=2.1.0",
